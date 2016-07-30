a1d66bfb02558955f908f7d3309eecde5963735f
janus_wel <janus.wel.3@gmail.com>
2016-07-30 13:48:14 +0900

    Fix package name to transform

--- transform.js
+++ transform.js
@@ -3,7 +3,7 @@ module.exports = function () {
         visitor: {
             ImportDeclaration: function(path) {
                 if (path.node.source.value === 'react-native-google-analytics-bridge') {
-                    path.node.source.value = 'react-native-google-analytics-bridge-node'
+                    path.node.source.value = 'react-native-google-analytics-bridge-mock'
                 }
             }
         }
